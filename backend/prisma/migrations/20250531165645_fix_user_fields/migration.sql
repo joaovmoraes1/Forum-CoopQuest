a592bad6c7833e385647dbda53b09daf2505c0a8
joaovmoraes1 <joaovitorrodriguesmoraes207@gmail.com>
2025-05-31 14:26:04 -0300

    fix: add missing user fields migration

---
+++ backend/prisma/migrations/20250531165645_fix_user_fields/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "GameRoom" DROP CONSTRAINT "GameRoom_hostId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "GameRoom" ADD CONSTRAINT "GameRoom_hostId_fkey" FOREIGN KEY ("hostId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
